777bbaed330b0a47ceb9ceecb6c3a23c61f5ec99
My-name-is-Jamshidbek <jamshidbekollanazarov@outlook.com>
2025-11-03 17:53:51 +0500

    fix: add logging for prediction confirmation in risk level determination

--- sensorapp/management/commands/analyze_sensors_ml.py
+++ sensorapp/management/commands/analyze_sensors_ml.py
@@ -43,6 +43,7 @@ class Command(BaseCommand):
             return "HIGH", "PANIC BUTTON PRESSED"
 
         if prediction == 1 and proba > 0.8 and (motion or door):
+            print(prediction)
             return "HIGH", "Confirmed by context sensors"
 
         if prediction == 1 and proba > 0.6:
